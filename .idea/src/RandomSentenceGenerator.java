2591f75e53575d56949e2e86e1a018a2def9cdbc
Whitman 208 <wh208@it.farmingdale.edu>
2026-02-13 13:27:48 -0500

    fixed up the original String error now dealing with conversion history error

--- .idea/src/RandomSentenceGenerator.java
+++ .idea/src/RandomSentenceGenerator.java
@@ -5,26 +5,24 @@ public class RandomSentenceGenerator {
 
         Random num = new Random();
 
-        String[] articleArray ={"The", "A", "One", "Some" , "Any"} ;
+        String[] articleArray1 ={"The", "A", "One", "Some" , "Any"} ;
+        String[] articleArray2 ={"the", "a", "one", "some", "any"};
         String[] nounArray = {"boy", "girl", "dog", "town", "car"};
         String[] verbArray = {"drove", "jumped", "ran", "walked", "skipped"};
         String[] prepositionArray = {"to", "from", "over", "under", "on"};
 
         for(int i = 0; i <= 20; i++){
-           /* System.out.print(articleArray[num.nextInt(0,4)] + " ");
+            System.out.print(articleArray1[num.nextInt(0,4)] + " ");
             System.out.print(nounArray[num.nextInt(0,4)] + " ");
             System.out.print(verbArray[num.nextInt(0,4)] + " ");
             System.out.print(prepositionArray[num.nextInt(0,4)] + " ");
-            System.out.print(articleArray[num.nextInt(0,4)] + " ");
-            System.out.print(nounArray[num.nextInt(0,4)] + "."); */
+            System.out.print(articleArray2[num.nextInt(0,4)] + " ");
+            System.out.print(nounArray[num.nextInt(0,4)] + ".\n");
         }
         return "\n";
     }
 
     public static void main(String[] args){
-        Random num = new Random();
-
-        System.out.println(num.nextInt(0,4));
 
         RandomSentenceGenerator();
 
