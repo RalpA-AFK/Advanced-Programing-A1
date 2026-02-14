919e4b94a517aab6413a6caf4aaaa2cf4551e1a2
Andrew Ralph <ralpaa@farmingdale.edu>
2026-02-13 19:22:48 -0500

    attempted to start dateformats and got lost while handling declerations

--- .idea/src/DateFormats.java
+++ .idea/src/DateFormats.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class DateFormats {
 
     public static void main(String[] args){
+    Scanner scnr = new Scanner(System.in);
+    System.out.println("Write a random date in the format 02/13/2026" );
+    String integerDate = scnr.nextLine();
+    scnr.close();
+    String[] months = {"January", "Febuary", "March", "April","May",
+    "June","July","August","September","October","November","December"};
+
+    for(int i = 0; i < integerDate.length();i++){
+    char cycler = integerDate.charAt(i);
+    while(i < 2){
+       String stringMonth = String.valueOf(integerDate.charAt(i));
 
     }
+    }
 
+    }
 }
+
