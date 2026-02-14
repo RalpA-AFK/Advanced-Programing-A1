f69daa1143a25feb0bc84bcdd48526de3dc67f6e
Andrew Ralph <ralpaa@farmingdale.edu>
2026-02-14 15:50:21 -0500

    was suprisingly simple phoneNumberConverter is complete

--- .idea/src/PhoneNumberConverter.java
+++ .idea/src/PhoneNumberConverter.java
@@ -2,6 +2,13 @@ import java.util.Scanner;
 public class PhoneNumberConverter {
 
     public static void main(String[] args){
+        Scanner scnr = new Scanner(System.in);
+        System.out.println("Type out a phone number in the form (555) 555-5555");
+        String phoneNumber = scnr.nextLine();
+        scnr.close();
+        String[] tokens = phoneNumber.split(" ");
+        System.out.println("Area code: " + tokens[0] + "\nNumber: " + tokens[1] + "\nPhone Number:" + phoneNumber);
+
 
     }
 }
