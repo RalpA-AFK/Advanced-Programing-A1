1fabc16a76e704c259a2f549de64382784457df3
Andrew Ralph <ralpaa@farmingdale.edu>
2026-02-14 17:32:22 -0500

    all assignments completed

--- .idea/src/PhoneNumberConverter.java
+++ .idea/src/PhoneNumberConverter.java
@@ -6,9 +6,13 @@ public class PhoneNumberConverter {
         System.out.println("Type out a phone number in the form (555) 555-5555");
         String phoneNumber = scnr.nextLine();
         scnr.close();
-        String[] tokens = phoneNumber.split(" ");
-        System.out.println("Area code: " + tokens[0] + "\nNumber: " + tokens[1] + "\nPhone Number:" + phoneNumber);
-
-
+        String[] tokens1 = phoneNumber.split("\\)");
+        String split1 = tokens1[0];
+        String[] tokens2 = split1.split("\\(");
+        String[] token = phoneNumber.split("-");
+        String split2 = token[0];
+        String[] token2 = split2.split(" ");
+        System.out.println("Area code: " + tokens2[1] + "\nNumber: " + token2[1] + token[1] + "\nPhone Number:" + phoneNumber);
     }
 }
+//(555) 555-5555
